228bd7b21aff678d50df1104147ff526af8b34fd
khayalnb <khayalnb@code.edu.az>
2021-08-07 19:31:41 +0400

    Blog page Contact part design fixed

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,8 @@
 $('.parallax-window').parallax({imageSrc: 'assets/image/Contact/maarten-deckers-0-frPETzEVs-unsplash.jpg'});
+
+
+function Btn(c){
+    c.style.borderColor="tan";
+    c.style.borderStyle="solid"
+    c.style.borderWidth ="3px"
+}
