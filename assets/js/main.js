68aed106db6e4dc634096b7356dd5d0c16507074
khayalnb <khayalnb@code.edu.az>
2021-08-11 14:57:27 +0400

    About page Navbar design fixed and index page Local storage basket system complate

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,33 @@ $('#simple_timer').syotimer({
   minute: 30
 
 });
+
+// let icon=document.querySelectorAll('.icon_hover')
+// let change=document.querySelector('.menu-change')
+
+
+
+
+// icon.forEach(element=>{
+//   element.addEventListener('click', ()=>{
+//     element.classList.toggle('rotateIcon');
+//     let change=document.querySelector('.menu-change');
+//     change.classList.toggle('menu-active')
+
+
+// })})
+
+
+$(".icon_hover").on("click", function () {
+  $(".icon_hover").children().not($(this)).removeClass('rotateIcon');
+  $(this).children().toggleClass("rotateIcon")
+  $(this).on("click", ()=>{
+    $(this).children().not($(this)).toggleClass('rotateIcon');
+  })
+  $(".icon_hover").next().not($(this).next()).removeClass('menu-active')
+  $(this).next().toggleClass("menu-active")
+})
+
+
+
+
